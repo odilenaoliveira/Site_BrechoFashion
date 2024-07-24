7b853ec4bd3e283fe98cc8f1855e255950a538bf
Odilena Oliveira <91203512+odilenaoliveira@users.noreply.github.com>
2024-07-24 08:57:26 +0100

    Version_0.0
    Site sobre vendas de roupas para brechó

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY>0);
+});
+
+const menuToggle = document.querySelector("menu-toggle");
+const navbar = document.querySelector("navbar");
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
