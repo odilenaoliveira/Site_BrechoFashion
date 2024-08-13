c057194f90fd09dc2c2b4e8c333cf9dc51e3e657
Odilena Oliveira <91203512+odilenaoliveira@users.noreply.github.com>
2024-08-13 08:45:34 +0100

    Site_BrechoFashion0_2
    - Alterando conteúdo banner
    - Adicionando links

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY>0);
 });
 
-const menuToggle = document.querySelector("menu-toggle");
-const navbar = document.querySelector("navbar");
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active');
     navbar.classList.toggle('active');
